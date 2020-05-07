bc571d75cf9663773018614906c3b5c4cbe610d5
Jason Coleman <jason@strangerstudios.com>
2020-05-07 11:23:35 -0400

    initial

---
+++ game.js
@@ -0,0 +1,107 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var player;
+var graphics;
+var cursors;
+var text;
+var moveCam = false;
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{    
+    this.load.atlas('farm', 'assets/farm.png', 'assets/farm.json');
+}
+
+function create ()
+{
+    //  Set the camera and physics bounds to be the size of 4x4 bg images
+    this.cameras.main.setBounds(-1024, -1024, 1024 * 2, 1024 * 2);
+    this.physics.world.setBounds(-1024, -1024, 1024 * 2, 1024 * 2);
+   
+    cursors = this.input.keyboard.createCursorKeys();
+
+    // Load farm sprites.
+    var atlasTexture = this.textures.get('farm');
+    var frames = atlasTexture.getFrameNames();  
+
+    player = this.physics.add.image(0, 0, 'farm', 'sprite244');
+    player.setScale(4);    
+
+    player.setCollideWorldBounds(true);
+
+    this.cameras.main.startFollow(player, true);
+
+    this.cameras.main.setDeadzone(400, 200);
+    this.cameras.main.setZoom(0.5);
+
+    if (this.cameras.main.deadzone)
+    {
+        graphics = this.add.graphics().setScrollFactor(0);
+        graphics.lineStyle(2, 0x00ff00, 1);
+        graphics.strokeRect(200, 200, this.cameras.main.deadzone.width, this.cameras.main.deadzone.height);
+    }
+
+    text = this.add.text(32, 32).setScrollFactor(0).setFontSize(32).setColor('#ffffff');;
+}
+
+function update ()
+{
+    var cam = this.cameras.main;
+
+    if (cam.deadzone)
+    {
+        text.setText([
+            'ScrollX: ' + cam.scrollX,
+            'ScrollY: ' + cam.scrollY,
+            'MidX: ' + cam.midPoint.x,
+            'MidY: ' + cam.midPoint.y,
+            'deadzone left: ' + cam.deadzone.left,
+            'deadzone right: ' + cam.deadzone.right,
+            'deadzone top: ' + cam.deadzone.top,
+            'deadzone bottom: ' + cam.deadzone.bottom
+        ]);
+    }
+    else
+    {
+        text.setText([
+            'ScrollX: ' + cam.scrollX,
+            'ScrollY: ' + cam.scrollY,
+            'MidX: ' + cam.midPoint.x,
+            'MidY: ' + cam.midPoint.y
+        ]);
+    }
+
+    player.setVelocity(0);
+
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-300);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(300);
+    }
+
+    if (cursors.up.isDown)
+    {
+        player.setVelocityY(-300);
+    }
+    else if (cursors.down.isDown)
+    {
+        player.setVelocityY(300);
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,9 @@
+# Nugget HTML5 Game
+
+## Resources
+
+1. Uses Phaser v3 https://phaser.io/phaser3
+
+1. Farm sprites from ?
+
+1. Built Sprite Atlas using https://www.leshylabs.com/apps/sstool/
